c68cd4f9da8a06cdb3d45698f69fa5e5e79f3931
iAntonova <antonovava@gmail.com>
2022-04-30 15:42:54 +0300

    add drivers

--- HW_01.py
+++ HW_01.py
@@ -1,1 +1,10 @@
+from lib2to3.pgen2 import driver
+from jmespath import search
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from jmespath import search
+driver = webdriver.Chrome()
+
+driver.get('http://automationpractice.com/index.php')
+searchbox = driver.find_element_by_xpath('//*[@id="search_query_top"]')
